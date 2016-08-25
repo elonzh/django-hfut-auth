2b857033ae6e8b41a9ff797ba946371531bb8c19
erliang <dev@erliang.me>
2016-08-25 13:58:43 +0800

    reset version

--- hfut_auth/__init__.py
+++ hfut_auth/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 __title__ = 'django-hfut-auth'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'erliang'
 __author_email__ = 'dev@erliang.me'
 __url__ = 'https://github.com/er1iang/django-hfut-auth'
